8a9b82d13f59cfc9ccbfc52373d5a0c0fb7745e8
wanderleysoliveira <wanderley_santos@ymail.com>
2021-07-19 22:30:19 -0300

    Criação do metodo para tocar a partir do input

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 document.body.addEventListener('keyup', (event)=> {
     playSound(event.code.toLocaleLowerCase())
+})
 
+document.querySelector('.composer button').addEventListener('click', () =>{
+    let song = document.querySelector('#input').value; // mesma coisa que document.getElementByID
+
+    if(song){
+        let songArray = song.split('');
+        playComposition(songArray);
+    }
 })
 
 function playSound(sound) {
@@ -8,14 +16,26 @@ function playSound(sound) {
     let keyElement = document.querySelector(`div[data-key="${sound}"]`)
 
     if(audioElement) {
+        audioElement.currentTime = 0;
         audioElement.play();
     }
 
     if(keyElement){
-        keyElement.classList.add('active'); 
+        keyElement.classList.add('active'); //Metodo para adicionar uma classe CSS a um elemento,
 
         setTimeout(()=> {
             keyElement.classList.remove('active');
         }, 300);
+    }
+}
+
+function playComposition(songArray){
+    let wait = 0;
+
+    for(let songItem of songArray) {
+        setTimeout(() => {
+            playSound(`key${songItem}`);
+        }, wait)
+        wait += 250;
     }
 }
